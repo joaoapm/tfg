67ccf7c566a2e00076ba0c84e2ad1930c02bc889
joaoapm <joaoapm@gmail.com>
2015-04-05 17:52:32 -0300

    etc

---
+++ Tfg/TrabDesen/Jogo/desktop/src/com/jogoTcc/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jogoTcc.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jogoTcc.Princinpal;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Princinpal(), config);
+	}
+}
