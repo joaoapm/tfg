35b7abd2e085d5e031ce3f418386cf587fd0a467
joaoapm <joaoapm@gmail.com>
2015-04-06 20:16:38 -0300

    Signed-off-by: joaoapm <joaoapm@gmail.com>

--- Tfg/TrabDesen/Jogo/core/src/com/jogoTcc/estrutura/Princinpal.java
+++ Tfg/TrabDesen/Jogo/core/src/com/jogoTcc/estrutura/Princinpal.java
@@ -1,11 +1,6 @@
 package com.jogoTcc.estrutura;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.jogoTcc.entidade.Mapa;
 
 public class Princinpal extends Game {
@@ -13,8 +8,6 @@ public class Princinpal extends Game {
 	@Override
 	public void create() {
 		setScreen(new Mapa());
-		// TODO Auto-generated method stub
-		
 	}
- 
+
 }
