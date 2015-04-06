77aab78d1c6fe40da6bc2eba8db41f12bd4cb629
joaoapm <joaoapm@gmail.com>
2015-04-05 23:34:43 -0300

    mov

--- Tfg/TrabDesen/Jogo/desktop/src/com/jogoTcc/desktop/Mapa.java
+++ Tfg/TrabDesen/Jogo/desktop/src/com/jogoTcc/desktop/Mapa.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
@@ -12,29 +14,35 @@ public class Mapa implements Screen {
 	private TiledMap map;
 
 	private OrthogonalTiledMapRenderer renderer;
-	
+
 	private OrthographicCamera camera;
+
+	private Jogador jogador;
 
 	@Override
 	public void show() {
 		TmxMapLoader loader = new TmxMapLoader();
 		map = loader.load("maps/mapa2.tmx");
-		
+
 		renderer = new OrthogonalTiledMapRenderer(map);
 		camera = new OrthographicCamera();
-		
-		
-	
+
+		jogador = new Jogador(new Sprite(new Texture("char.png")));
+		Gdx.input.setInputProcessor(jogador);
+
 	}
 
 	@Override
-	public void render(float delta) { 
+	public void render(float delta) {
 		// TODO Auto-generated method stub
 		Gdx.gl30.glClearColor(0, 0, 0, 1);
 		Gdx.gl30.glClear(GL30.GL_COLOR_BUFFER_BIT);
 		renderer.setView(camera);
 		renderer.render();
 
+		renderer.getBatch().begin();
+		jogador.draw(renderer.getBatch());
+		renderer.getBatch().end();
 	}
 
 	@Override
