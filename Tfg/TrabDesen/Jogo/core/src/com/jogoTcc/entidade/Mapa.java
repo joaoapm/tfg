3638980fe9272ebb540622d507744933a6dc2370
joaoapm <joaoapm@gmail.com>
2015-04-17 21:30:42 -0300

    novojogo

--- Tfg/TrabDesen/Jogo/core/src/com/jogoTcc/entidade/Mapa.java
+++ Tfg/TrabDesen/Jogo/core/src/com/jogoTcc/entidade/Mapa.java
@@ -8,22 +8,33 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.jogoTcc.estrutura.Partida;
 
 public class Mapa extends Stage{
 
-	private TiledMap mapa;
+	public static  TiledMap mapa;
 
 	public static IsometricTiledMapRenderer renderizador;
 
-	private OrthographicCamera camera;
+	public static OrthographicCamera camera;
 
 	public Mapa(String nomeMapa) {
 		TmxMapLoader loader = new TmxMapLoader();
 		this.mapa = loader.load(nomeMapa);
 
+		Matrix4 isoTransform = new Matrix4();
+		isoTransform.idt();
+
+		  isoTransform.translate(10, 32,10);
+		isoTransform.scale((float)(Math.sqrt(2.0) / 2.0), (float)(Math.sqrt(2.0) / 4.0), 1.0f);
+		isoTransform.rotate(10.0f, 0.0f, 1.0f, -45);
 		
+		super.getBatch().setTransformMatrix(isoTransform);
+ 
+		super.getBatch().setProjectionMatrix(isoTransform);
 		 for (MapLayer layer : this.mapa.getLayers()) {
 	            TiledMapTileLayer tiledLayer = (TiledMapTileLayer)layer;
 	            createActorsForLayer(tiledLayer);
@@ -35,42 +46,33 @@ public class Mapa extends Stage{
 	}
 
 	public void renderizarMapa() {
-	//	Gdx.gl30.glClearColor(0, 0, 0, 1);
-	//	Gdx.gl30.glClear(GL30.GL_COLOR_BUFFER_BIT);
-	//	this.renderizador.setView(camera);
-	//	this.renderizador.render();
+	 	Gdx.gl30.glClearColor(0, 0, 0, 1);
+	 	Gdx.gl30.glClear(GL30.GL_COLOR_BUFFER_BIT);
+	 	this.renderizador.setView(camera);
+	 	this.renderizador.render();
 
 	}
 
 	private void createActorsForLayer(TiledMapTileLayer tiledLayer) {
-		for (int x = 0; x < tiledLayer.getWidth(); x++) {
-			for (int y = 0; y < tiledLayer.getHeight(); y++) {
-				TiledMapTileLayer.Cell cell = tiledLayer.getCell(x, y);
+		//for (int x = 0; x < tiledLayer.getWidth(); x++) {
+		//	for (int y = 0; y < tiledLayer.getHeight(); y++) {
+				TiledMapTileLayer.Cell cell = tiledLayer.getCell(0, 0);
 				TiledMapActor actor = new TiledMapActor(this.mapa, tiledLayer,cell);
-				actor.setBounds(x * tiledLayer.getTileWidth(),
-						y * tiledLayer.getTileHeight(),
-						tiledLayer.getTileWidth(), tiledLayer.getTileHeight());
-				addActor(actor);
+				actor.setBounds(220 +  tiledLayer.getTileWidth(),
+						-240 +  tiledLayer.getTileHeight(),
+						38,38);
+				//addActor(actor);
+				actor.setTransform(true);
+			 
+				Partida.gp.setRotation(45);
+				Partida.gp.addActor(actor);
+			 
 				EventListener eventListener = new TiledMapClickListener(actor);
 				actor.addListener(eventListener);
-			}
-		}
+		//	}
+		//}
 	}
 
-	public IsometricTiledMapRenderer getRenderizador() {
-		return renderizador;
-	}
-
-	public void setRenderizador(IsometricTiledMapRenderer renderizador) {
-		this.renderizador = renderizador;
-	}
-
-	public OrthographicCamera getCamera() {
-		return camera;
-	}
-
-	public void setCamera(OrthographicCamera camera) {
-		this.camera = camera;
-	}
+ 
 
 }
--- Tfg/TrabDesen/Jogo/core/src/com/jogoTcc/estrutura/Partida.java
+++ Tfg/TrabDesen/Jogo/core/src/com/jogoTcc/estrutura/Partida.java
@@ -5,10 +5,11 @@ import java.util.ArrayList;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL30;
-import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.jogoTcc.entidade.Entidade;
 import com.jogoTcc.entidade.Jogador;
 import com.jogoTcc.entidade.Mapa;
+import com.sun.prism.image.ViewPort;
 
 public class Partida implements Screen {
 
@@ -23,11 +24,12 @@ public class Partida implements Screen {
 		mapa = new Mapa("mapa/mapa.tmx");
 		Gdx.input.setInputProcessor(mapa);
 		
-		// inicializa jogador humano com 5 bonecos
-		jogador = new Jogador(TipoJogador.COMPUTADOR, 1, mapa);
-
+		 ViewPort vv = new ViewPort(0, 0, 100, 100);
+  
 	}
 
+	public static Group gp = new Group();
+	
 	@Override
 	public void render(float delta) {
 
@@ -35,13 +37,11 @@ public class Partida implements Screen {
 		Gdx.gl30.glClearColor(0, 0, 0, 1);
 		Gdx.gl30.glClear(GL30.GL_COLOR_BUFFER_BIT);
 		//mapa.renderizarMapa();
-		
-		Mapa.renderizador.setView(mapa.getCamera());
 		Mapa.renderizador.render();
+		mapa.addActor(this.gp);
 		// atualiza palco
-		mapa.act(Gdx.graphics.getDeltaTime());
-		mapa.draw();
-	 
+		 mapa.act(Gdx.graphics.getDeltaTime());
+		 mapa.draw();
 		// desenha mapa
 		// this.mapa.renderizarMapa();
 
